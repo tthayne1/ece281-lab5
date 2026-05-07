dcd87e2624f854f02150659b040991b775a3e923
tthayne1 <253853281+tthayne1@users.noreply.github.com>
2026-05-07 10:29:25 -0600

    plssss

--- src/ALU.vhd
+++ src/ALU.vhd
@@ -1,4 +1,3 @@
-----------------------------------------------------------------------------------
 -- Company: 
 -- Engineer: 
 -- 
@@ -18,18 +17,9 @@
 -- 
 ----------------------------------------------------------------------------------
 
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
 use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity ALU is
     Port ( i_A : in STD_LOGIC_VECTOR (7 downto 0);
@@ -41,51 +31,69 @@ end ALU;
 
 architecture Behavioral of ALU is
 
-    signal w_result : STD_LOGIC_VECTOR (7 downto 0);
-    signal w_carry  : STD_LOGIC;
-    signal w_overflow : STD_LOGIC;
-
 begin
 
     process(i_A, i_B, i_op)
         variable v_temp : unsigned(8 downto 0);
+        variable v_result : STD_LOGIC_VECTOR(7 downto 0);
+        variable v_N : STD_LOGIC;
+        variable v_Z : STD_LOGIC;
+        variable v_C : STD_LOGIC;
+        variable v_V : STD_LOGIC;
     begin
-        w_carry <= '0';
-        w_overflow <= '0';
+        v_result := (others => '0');
+        v_N := '0';
+        v_Z := '0';
+        v_C := '0';
+        v_V := '0';
 
         case i_op is
             when "000" =>
-                v_temp := ('0' & unsigned(i_A)) + ('0' & unsigned(i_B));
-                w_result <= std_logic_vector(v_temp(7 downto 0));
-                w_carry <= v_temp(8);
+                v_temp := resize(unsigned(i_A), 9) + resize(unsigned(i_B), 9);
+                v_result := std_logic_vector(v_temp(7 downto 0));
+                v_C := v_temp(8);
+
+                if (i_A(7) = i_B(7)) and (v_result(7) /= i_A(7)) then
+                    v_V := '1';
+                end if;
 
             when "001" =>
-                v_temp := ('0' & unsigned(i_A)) - ('0' & unsigned(i_B));
-                w_result <= std_logic_vector(v_temp(7 downto 0));
+                v_temp := resize(unsigned(i_A), 9) - resize(unsigned(i_B), 9);
+                v_result := std_logic_vector(v_temp(7 downto 0));
 
                 if unsigned(i_A) >= unsigned(i_B) then
-                    w_carry <= '1';
+                    v_C := '1';
                 else
-                    w_carry <= '0';
+                    v_C := '0';
+                end if;
+
+                if (i_A(7) /= i_B(7)) and (v_result(7) /= i_A(7)) then
+                    v_V := '1';
                 end if;
 
             when "010" =>
-                w_result <= i_A and i_B;
+                v_result := i_A and i_B;
 
             when "011" =>
-                w_result <= i_A or i_B;
+                v_result := i_A or i_B;
 
             when others =>
-                w_result <= (others => '0');
+                v_result := (others => '0');
         end case;
+
+        v_N := v_result(7);
+
+        if v_result = "00000000" then
+            v_Z := '1';
+        else
+            v_Z := '0';
+        end if;
+
+        o_result <= v_result;
+        o_flags(3) <= v_N;
+        o_flags(2) <= v_Z;
+        o_flags(1) <= v_C;
+        o_flags(0) <= v_V;
     end process;
-
-    o_result <= w_result;
-
-    -- Flags: [3]=Negative, [2]=Zero, [1]=Carry, [0]=Overflow
-    o_flags(3) <= w_result(7);
-    o_flags(2) <= '1' when w_result = "00000000" else '0';
-    o_flags(1) <= w_carry;
-    o_flags(0) <= w_overflow;
 
 end Behavioral;
