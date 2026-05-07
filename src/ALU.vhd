eb83317cd7f8e544e64fe77de5aa6f6bacf4bba3
tthayne1 <253853281+tthayne1@users.noreply.github.com>
2026-05-06 21:47:30 -0600

    alu fix

--- src/ALU.vhd
+++ src/ALU.vhd
@@ -42,20 +42,34 @@ end ALU;
 architecture Behavioral of ALU is
 
     signal w_result : STD_LOGIC_VECTOR (7 downto 0);
+    signal w_carry  : STD_LOGIC;
+    signal w_overflow : STD_LOGIC;
 
 begin
 
     process(i_A, i_B, i_op)
+        variable v_temp : unsigned(8 downto 0);
     begin
+        w_carry <= '0';
+        w_overflow <= '0';
+
         case i_op is
             when "000" =>
-                w_result <= std_logic_vector(signed(i_A) + signed(i_B));
+                v_temp := ('0' & unsigned(i_A)) + ('0' & unsigned(i_B));
+                w_result <= std_logic_vector(v_temp(7 downto 0));
+                w_carry <= v_temp(8);
+
             when "001" =>
-                w_result <= std_logic_vector(signed(i_A) - signed(i_B));
+                v_temp := ('0' & unsigned(i_A)) - ('0' & unsigned(i_B));
+                w_result <= std_logic_vector(v_temp(7 downto 0));
+                w_carry <= not v_temp(8);
+
             when "010" =>
                 w_result <= i_A and i_B;
+
             when "011" =>
                 w_result <= i_A or i_B;
+
             when others =>
                 w_result <= (others => '0');
         end case;
@@ -66,7 +80,7 @@ begin
     -- Flags: [3]=Negative, [2]=Zero, [1]=Carry, [0]=Overflow
     o_flags(3) <= w_result(7);
     o_flags(2) <= '1' when w_result = "00000000" else '0';
-    o_flags(1) <= '0';
-    o_flags(0) <= '0';
+    o_flags(1) <= w_carry;
+    o_flags(0) <= w_overflow;
 
 end Behavioral;
